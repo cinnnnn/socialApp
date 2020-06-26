c80adb2c41d1022e7bd195ef3d3782c776119e4b
Denis Shchigolev <denis.shchigolev@yandex.ru>
2020-06-26 13:14:36 +0300

    Initial Commit

---
+++ socialApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  socialApp
+//
+//  Created by Денис Щиголев on 26.06.2020.
+//  Copyright © 2020 Денис Щиголев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
