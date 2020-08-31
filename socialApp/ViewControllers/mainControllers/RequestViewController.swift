51b8adc45d5dc88beb0624450934ee4683d58c90
Denis Shchigolev <denis.shchigolev@yandex.ru>
2020-08-31 17:24:14 +0300

    request view controller

--- socialApp/ViewControllers/mainControllers/RequestViewController.swift
+++ socialApp/ViewControllers/mainControllers/RequestViewController.swift
@@ -1,41 +1,34 @@
 //
-//  UserViewController.swift
+//  RequestViewController.swift
 //  socialApp
 //
-//  Created by Денис Щиголев on 29.08.2020.
+//  Created by Денис Щиголев on 31.08.2020.
 //  Copyright © 2020 Денис Щиголев. All rights reserved.
 //
 
 import UIKit
 import SwiftUI
 
-class UserViewController: UIViewController {
+class RequestViewController: UIViewController {
     
     let photo = UIImageView(image: #imageLiteral(resourceName: "Photo3"), contentMode: .scaleAspectFill)
     let container = UIView()
     let nameLabel = UILabel(labelText: "Зельда", textFont: .boldSystemFont(ofSize: 22))
     let messageTextView = UITextView(text: "Легенды Зельды", isEditableText: false, delegate: nil)
-    let unswerTextField = OneLineTextField(isSecureText: false, withButton: true, buttonText: "Отправить", placeHoledText: "Сообщение")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        configure()
-        setupConstraints()
         setupAction()
-
+        setupConstraints()
+        configure()
+        
     }
     
     //MARK: - setupAction()
     private func setupAction() {
         
-        if let sendButton = unswerTextField.rightView as? UIButton {
-            sendButton.addTarget(self, action: #selector(sendMessage), for: .touchUpInside)
-        }
-    }
-    
-    @objc func sendMessage() {
-        print("Olololo")
+        
     }
     
     //MARK: - configure()
@@ -45,22 +38,20 @@ class UserViewController: UIViewController {
         container.layer.cornerRadius = 30
     }
     
-       //MARK: - setupConstraints()
+    //MARK: - setupConstraints()
     private func setupConstraints() {
         
         photo.translatesAutoresizingMaskIntoConstraints = false
         container.translatesAutoresizingMaskIntoConstraints = false
         nameLabel.translatesAutoresizingMaskIntoConstraints = false
         messageTextView.translatesAutoresizingMaskIntoConstraints = false
-        unswerTextField.translatesAutoresizingMaskIntoConstraints = false
-    
-        
+      
         view.addSubview(photo)
         view.addSubview(container)
         container.addSubview(nameLabel)
         container.addSubview(messageTextView)
-        container.addSubview(unswerTextField)
-      
+     
+        
         NSLayoutConstraint.activate([
             
             //main constraints
@@ -81,33 +72,28 @@ class UserViewController: UIViewController {
             
             messageTextView.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 30),
             messageTextView.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 30),
-            messageTextView.trailingAnchor.constraint(equalTo: container.trailingAnchor, constant: -30),
+            messageTextView.trailingAnchor.constraint(equalTo: container.trailingAnchor, constant: -30)
             
-            unswerTextField.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 30),
-            unswerTextField.trailingAnchor.constraint(equalTo: container.trailingAnchor, constant: -30),
-            unswerTextField.bottomAnchor.constraint(equalTo: container.bottomAnchor, constant: -60),
-            unswerTextField.heightAnchor.constraint(equalToConstant: 40)
+            
         ])
     }
 }
 
+
 //MARK: - SwiftUI
-struct UserViewControllerProvider: PreviewProvider {
-   
+struct RequestViewControllerProvider: PreviewProvider {
+    
     static var previews: some View {
         ContenerView().edgesIgnoringSafeArea(.all)
     }
     
     struct ContenerView: UIViewControllerRepresentable {
         
-        
-        typealias UIViewControllerType = UserViewController
-        
-        func makeUIViewController(context: Context) -> ContenerView.UIViewControllerType {
-            UserViewController()
+        func makeUIViewController(context: Context) -> RequestViewController {
+            RequestViewController()
         }
         
-        func updateUIViewController(_ uiViewController: UserViewController, context: Context) {
+        func updateUIViewController(_ uiViewController: RequestViewController, context: Context) {
             
         }
     }
