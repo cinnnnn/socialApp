6746b2b5e379d1b96034b8d3e351bb0f54d52bd6
Denis Shchigolev <denis.shchigolev@yandex.ru>
2020-09-16 23:33:46 +0300

    check2 delete google.plist

--- socialApp/ViewControllers/mainControllers/SetProfileViewController.swift
+++ socialApp/ViewControllers/mainControllers/SetProfileViewController.swift
@@ -305,7 +305,8 @@ extension SetProfileViewController: UITextFieldDelegate {
 }
 //MARK:  UITextViewDelegate
 extension SetProfileViewController:UITextViewDelegate {
-//
+
+    
 //    func textViewDidBeginEditing(_ textView: UITextView) {
 //        if textView.text == "Для просмотра обьявлений других пользователей, расскажи о своих желаниях..." {
 //            textView.text = ""
